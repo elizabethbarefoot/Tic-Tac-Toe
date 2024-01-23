0c8fc4a345cdfb32e51df103d1fd2be31a5a909d
Elizabeth Barefoot <elizabeth.barefoot@mrjaffesclass.com>
2024-01-23 11:40:39 -0800

    third commit

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -12,11 +12,51 @@ public class State
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
 
     public boolean isWinner() {
-      // You will write this code!!
+        // You will write this code!!
     }
 
     public boolean isTie() {
-      // You will write this code too!!
+        // You will write this code too!!
     }
-}
 
+    public int getGameState() {
+        return this.gameState;
+    }
+
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+
+    public int getWhoseMove() {
+        return this.whoseMove;
+    }
+
+    public void setWhoseMove() {
+        this.whoseMove = whoseMove;
+    }
+
+    public String getXName() {
+        return this.xName;
+    }
+
+    public void setXName(){
+        this.xName = xName;
+    }
+
+    public String getOName() {
+        return this.oName;
+    }
+
+    public void setOName(){
+        this.oName = oName;
+    }
+
+    public int getBoardCell(int row, int col) {
+        return this.board[row][col];
+    }
+
+    public void setBoardCell(int row, int col, int value) {
+        this.board[row][col] = value;
+    }
+
+}
