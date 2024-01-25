8cb5f35e59527415a86213da3ad7090f652e4267
Elizabeth Barefoot <elizabeth.barefoot@mrjaffesclass.com>
2024-01-25 11:45:51 -0800

    fourth commit

--- tictactoe/State.java
+++ tictactoe/State.java
@@ -12,18 +12,40 @@ public class State
     private int[][] board = new int[Constants.BOARD_SIZE][Constants.BOARD_SIZE];
 
     public boolean isWinner() {
-        // You will write this code!!
+        boolean isWinner = false;
+        int rowCount;
+        int columnCount;
+        int diagonalCount;
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            rowCount = 0;
+            for (int c = 0; c < BOARD_SIZE; c++) {
+                rowCount += board[i][c];
+            } 
+            if (rowCount == 3 * X_VALUE || rowCount == 3 * O_VALUE) {
+                isWinner = true;
+            }
+        }
+        
+        for (int i = 0; i < BOARD_SIZE; i++) {
+            coulmnCount = 0;
+            for (int c = 0; c < BOARD_SIZE; c++) {
+                columnCount += board[c][i];
+            } 
+            if (columnCount == 3 * X_VALUE || columnCount == 3 * O_VALUE) {
+                isWinner = true;
+            }
+        }
     }
+    
+        public boolean isTie() {
+            // You will write this code too!!
+        }
 
-    public boolean isTie() {
-        // You will write this code too!!
-    }
+        public int getGameState() {
+            return this.gameState;
+        }
 
-    public int getGameState() {
-        return this.gameState;
-    }
-
-    public void setGameState(int gameState) {
+        public void setGameState(int gameState) {
         this.gameState = gameState;
     }
 
